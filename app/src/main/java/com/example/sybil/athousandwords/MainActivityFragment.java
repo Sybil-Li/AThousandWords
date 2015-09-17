8618d5f4e5f0670e44a257d80adef8c9a622abb1
Sybil_Li <li.bing.qing.2009@vjc.sg>
2015-09-17 23:40:05 +0100

    Finished adding basic listview

--- app/src/main/java/com/example/sybil/athousandwords/MainActivityFragment.java
+++ app/src/main/java/com/example/sybil/athousandwords/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.example.sybil.athousandwords;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> displayEntriesAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,31 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        //TODO customize list to show both picture and text
+        displayEntriesAdapter =
+                new ArrayAdapter<String>(
+                        getActivity(), // The current context (this activity)
+                        R.layout.list_item_entries, // The name of the layout ID.
+                        R.id.list_item_entries_view, // The ID of the textview to populate.
+                        weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_entries);
+        listView.setAdapter(displayEntriesAdapter);
+
+        return rootView;
     }
 }
