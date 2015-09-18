613ce5777a498ef702b2880c9924b56cf8d77d2d
Sybil_Li <li.bing.qing.2009@vjc.sg>
2015-09-18 13:48:20 +0100

    Finished building custom listview

--- app/src/main/java/com/example/sybil/athousandwords/DisplayEntriesFragment.java
+++ app/src/main/java/com/example/sybil/athousandwords/DisplayEntriesFragment.java
@@ -8,10 +8,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -26,29 +22,25 @@ public class DisplayEntriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        String[] data = {
-                "Mon 6/23 - Sunny - 31/17",
-                "Tue 6/24 - Foggy - 21/8",
-                "Wed 6/25 - Cloudy - 22/17",
-                "Thurs 6/26 - Rainy - 18/11",
-                "Fri 6/27 - Foggy - 21/10",
-                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
-                "Sun 6/29 - Sunny - 20/7"
+        String[] description = {
+                "xin8ji",
+                "xin9ji"
+        } ;
+
+        Integer[] imageId = {
+                R.drawable.image1,
+                R.drawable.image2
         };
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
 
         //TODO customize list to show both picture and text
-        displayEntriesAdapter =
-                new ArrayAdapter<String>(
-                        getActivity(), // The current context (this activity)
-                        R.layout.list_item_entries, // The name of the layout ID.
-                        R.id.list_item_entries_view, // The ID of the textview to populate.
-                        weekForecast);
+        CustomList adapter = new
+                CustomList(getActivity(), description, imageId);
+
 
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
         ListView listView = (ListView) rootView.findViewById(R.id.listview_entries);
-        listView.setAdapter(displayEntriesAdapter);
+        listView.setAdapter(adapter);
+        //list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
         return rootView;
     }
